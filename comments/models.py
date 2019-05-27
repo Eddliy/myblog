1994252f73253fa304d6d12019525149cf0fe062
Eddliy <2794367857@qq.com>
2019-05-27 13:22:06 +0800

    add my blog porject to local rrepo.

---
+++ comments/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class Comment(models.Model):
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255)
+    text = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text[:20]
+
+    class Meta:
+        ordering = ['-created_time']
