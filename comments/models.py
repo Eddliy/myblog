eede1d05295f1104ea6b4974b69d11fc538505f8
Eddliy <2794367857@qq.com>
2019-05-28 21:10:51 +0800

    使用富文本编辑器使评论的功能更加强大

--- comments/models.py
+++ comments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 
 # Create your models here.
@@ -6,7 +7,8 @@ class Comment(models.Model):
 
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=255)
-    text = models.TextField()
+    # text = models.TextField()
+    text = RichTextField()
     created_time = models.DateTimeField(auto_now_add=True)
 
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE)
