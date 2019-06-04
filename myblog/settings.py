45e39f3030574e95a049f7c85fa2e28019b38d22
Eddliy <2794367857@qq.com>
2019-06-04 14:32:01 +0800

    细节改动

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3nf#581+cd=1f9pb6%tsgq_gc=3msjmg!!iy2n63*pjbh6qnc='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '111.67.197.68']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.zzxlsh.fun']
 
 
 # Application definition
